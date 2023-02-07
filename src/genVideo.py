d6e971e2bf52405297a7f0288678534f2ade9b5c
chaijinsong <chaijinsong@didiglobal.com>
2023-02-07 10:39:30 +0800

    feature:基本流程完成

--- src/genVideo.py
+++ src/genVideo.py
@@ -11,8 +11,6 @@ def create_video_from_images(output_path, image_input_dir, frame_rate=24.0, imag
 
     audio_clip = AudioFileClip(audio_path)
     every_duration = audio_clip.duration / len(image_paths)
-    audio = afx.audio_loop( audio_clip, duration=audio_clip.duration)
-    audio_clip_add = CompositeAudioClip([audio])
 
     # 必须保证图片的尺寸完全一致生成的视频才会正常，否则视频会闪动有噪音
     for path in image_paths:
@@ -21,23 +19,23 @@ def create_video_from_images(output_path, image_input_dir, frame_rate=24.0, imag
         clips.append(frame)
 
     final_video = concatenate_videoclips(clips, method='chain')
-    final_video.set_audio(audio_clip_add)
     final_video.write_videofile(output_path, fps=frame_rate, codec='libx264')
 
 # 先生成video，然后再给video加audio
 video_path = '../static/video/许嵩_放肆.mp4'
 img_path = '../static/imgs'
-audio_path = '../static/audio/放肆_许嵩.flac'
-create_video_from_images(video_path, '../static/imgs', audio_path=audio_path);
+audio_path = '../static/audio/许嵩_放肆.flac'
+print('图片生成视频 start')
+create_video_from_images(video_path, '../static/imgs', frame_rate=1, audio_path=audio_path);
+print('图片生成视频 end')
 
 def add_audio_to_video(video_path, audio_path):
     audio_clip = AudioFileClip(audio_path)
     video_clip = VideoFileClip(video_path)
-    print('audio时长：', audio_clip.duration)
-    audio = afx.audio_loop( audio_clip, duration=video_clip.duration)
 
-    audio_clip_add = CompositeAudioClip([audio])
-    final_video = video_clip.set_audio(audio_clip_add)
+    final_video = video_clip.set_audio(audio_clip)
     final_video.write_videofile(video_path);
 
-# add_audio_to_video(video_path, audio_path)
+print('给视频添加音频 start')
+add_audio_to_video(video_path, audio_path)
+print('给视频添加音频 end')
